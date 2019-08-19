30ce542911686ede27ba71a6c66ae1b94d2f8a8e
Jesse <jackjsj@foxmail.com>
2019-08-19 23:14:11 +0800

    小程序Demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,141 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+// pages/list/list.js
+import Mock from '../../miniprogram_npm/mockjs/index.js';
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    swipers: [],
+    movies: [],
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  onMovieCardTap(e) {
+    console.log(e);
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+
+  getMovies() {
+    wx.request({
+      url: 'https://douban.uieee.com/v2/movie/top250?count=9',
+      header: {
+        "Content-Type": "json"
+      },
+      success: (resp) => {
+        const {
+          subjects
+        } = resp.data;
+        
+        const movies = subjects.map(item => {
+          return {
+            name: item.title,
+            img: item.images.large,
+            score: item.rating.average,
+            
+          }
+        })
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          movies,
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
+  },
+
+  getInTheaters() {
+    wx.request({
+      url: 'https://douban.uieee.com/v2/movie/in_theaters?count=6',
+      header: {
+        "Content-Type": "json"
+      },
+      success: (resp) => {
+        const {
+          subjects
+        } = resp.data;
+        
+        const swipers = subjects.map(item=>{
+          wx.request({
+            url: 'https://douban.uieee.com/v2/movie/subject/'+item.id,
+            header: {
+              "Content-Type": "json"
+            },
+            success:(resp=>{
+              const {
+                summary,id
+              } = resp.data;
+              const target = this.data.swipers.filter(item=>item.id === id)[0]
+              target.summary = summary;
+              this.setData({
+                swipers:this.data.swipers,
+              })
+            })
+          })
+          return {
+            id: item.id,
+            img: item.images.large,
+            title: item.title,
+            score: item.rating.average,
+            casts: item.casts.map(c => c.name),
+          }
+        });
+        this.setData({
+          swipers,
+        })
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    // console.log(this.data.imgs);
+    this.getMovies();
+    this.getInTheaters();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
   }
 })
